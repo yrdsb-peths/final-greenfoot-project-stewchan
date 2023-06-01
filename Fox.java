2d30c290e2e4d923a36b00c19cdeb2e0d2b3a12a
Stewart Chan <stewart.chan@gapps.yrdsb.ca>
2023-06-01 08:15:29 -0400

    Animated fox

--- Fox.java
+++ Fox.java
@@ -7,17 +7,39 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Fox extends Actor
 {
     GreenfootImage[] images = new GreenfootImage[10];
+    SimpleTimer animTimer = new SimpleTimer();
 
     public Fox(){
         // Load images    
         for(int i = 0; i < 10; i++){
             images[i] = new GreenfootImage("images/fox/fox_0" + i + ".png");
+            images[i].scale(50,50);
         }
         setImage(images[0]);
+        animTimer.mark();
+    }
+
+    private int animIdx = 0;
+    public void animate(){
+        if(animTimer.millisElapsed() < 100) {
+            return;
+        }
+        animTimer.mark();
+        animIdx = (animIdx + 1) % images.length - 1;
+        setImage(images[++animIdx]);
+        
     }
 
     public void act()
     {
         // Add your action code here.
+        animate();
+        
+        if(Greenfoot.isKeyDown("a")){
+            move(-2);
+        }
+        if(Greenfoot.isKeyDown("d")){
+            move(2);
+        }
     }
 }
--- MainWorld.java
+++ MainWorld.java
@@ -1,21 +1,16 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
+ * The main game world.
  * 
- * @author (your name) 
- * @version (a version number or a date)
  */
 public class MainWorld extends World
 {
-
-    /**
-     * Constructor for objects of class MyWorld.
-     * 
-     */
     public MainWorld()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
+        
+        Fox fox = new Fox();
+        addObject(fox, getWidth()/2, getHeight()/2);
     }
 }
